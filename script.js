03da00494735729b45c7526ac99e31d3ee56f0b8
A. Taylor <alecia.taylor0@gmail.com>
2025-04-04 16:18:42 +0000

    Add temperature conversion options and improve result display in Unit Converter

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-
 function convert() {
   const inputValue = document.getElementById("userInput").value;
-  const unit = document.getElementById("unit");
-  const milesToKm = unit.value === "milesToKm";
+  const unit = document.getElementById("unit").value;
   let result = 0;
+  let units = "";
 
-  if (milesToKm) {
+  if (unit === "milesToKm") {
     result = inputValue * 1.60934;
-  } else {
+    units = "km";
+  } else if (unit === "kmToMiles") {
     result = inputValue / 1.60934;
+    units = "miles";
+  } else if (unit === "FtoC") {
+    result = (inputValue - 32) * 5 / 9;
+    units = "Celsius";
   }
+  else if (unit === "CtoF") {
+  result = (inputValue * 9 / 5) + 32;
+  units = "Fahrenheit";
+}
 
-  const resultString = inputValue + " units are " + result + " converted units";
+  const resultString = inputValue + " units are " + result + " " + units;
   const resultElement = document.getElementById('resultElement');
   resultElement.innerHTML = resultString;
+  
   console.log(resultString);
 }
